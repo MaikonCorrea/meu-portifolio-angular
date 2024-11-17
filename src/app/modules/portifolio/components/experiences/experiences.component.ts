ebdd59ad69dad3a9e204321e725ecffb593ac0ba
maikoncorrea <maikonacorrea@gmail.com>
2024-11-17 19:43:23 -0300

    adds experiences component, basic structure and unit tests

---
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { IExperiences } from '../../interface/IExperiences.interface';
+
+@Component({
+  selector: 'app-experiences',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.css'
+})
+export class ExperiencesComponent {
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      sumary: {
+        strong:"Tester de Software",
+        p: " Empresa: Genesys Engenharia de Software | Período: Janeiro 2024 - atualmente"
+      },
+      text: " Atuo na área de testes de software com foco em automação de testes, utilizando ferramentas como TestComplete. Além disso, realizo abertura de correções de bugs, testes manuais e automatizados, elaboração de planos de testes e documentação de testes."
+    }
+  ])
+}
